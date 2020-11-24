0a262ab4fe20f93054ff0ee3d1b771604516385e
Ryan EM <ryan.eccleston.murdock@gmail.com>
2020-11-24 13:22:45 -0500

    figured out why 2nd loop wasnt working --> readers only run through once

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,8 @@
 # 28 November 2020 
 # 
 # Purpose: Analyze .csv financial data 
+#
+# Sources: 
 
 import os
 import csv
@@ -13,14 +15,18 @@ import random
 path = 'Resources'
 csvpath = os.path.join(path, 'budget_data.csv')
 
-with open(csvpath) as inFile:
+with open(csvpath, 'r') as inFile:
+
 	budget_sheet = csv.reader(inFile, delimiter=',')
+	next(budget_sheet)
 
-	total_months = sum(1 for row in budget_sheet) - 1
-	print(total_months)
-	print('milk')
-	# header = next(budget_sheet)
-
+	total_months = 0
 	net = 0
+	current_month = ''
+	
 	for row in budget_sheet:
-		print(row)
+		total_months += 1
+		net += int(row[1])
+
+	print(total_months)
+	print(net)
