7ea7d35970c6029356419771bed1c493a5ec06ae
Ryan EM <ryan.eccleston.murdock@gmail.com>
2020-11-27 23:02:59 -0500

    mostly finished... fix sig figs... figure out if need to write to summary

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,13 +17,12 @@ out_path = 'analysis'
 out_file_name = ''
 out_csvpath = os.path.join(out_path, out_file_name)
 
-def candidateVoteCount(candidate, data):
+def candidateVoteCount(candidate, names):
 
 	votes = 0 
-	print(candidate)
-	for vote in data:
-		print(vote)
-		if candidate == vote[2]:
+
+	for name in names:
+		if candidate == name:
 			votes += 1
 
 	return votes
@@ -35,17 +34,36 @@ with open(in_csvpath, 'r') as inFile:
 
 	total_votes = 0
 	candidates = []
+	votes_per_candidate = []
+	name_instances = []
 
 	for vote in election_data:
 		total_votes += 1 
+		name_instances.append(vote[2])
 		if vote[2] not in candidates:
 			candidates.append(vote[2])
 
 	for candidate in candidates:
-		print(candidateVoteCount(candidate, election_data))
+		votes_per_candidate.append(candidateVoteCount(candidate, name_instances))
 		
 
-
-
+	print('Election Results')
+	print('--------------------------')
 	print('Total Votes: ', total_votes)
-	print(candidates)
+	print('--------------------------')
+
+	for candidate in candidates:
+		candidate_index = candidates.index(candidate)
+		votes_recieved = votes_per_candidate[candidate_index] 
+
+		percent_votes = round(100 * (votes_recieved / total_votes), 3)
+		print(candidate, ': ', percent_votes, '% (', votes_recieved, ')')
+
+	print('--------------------------')
+	winner_index = votes_per_candidate.index(max(votes_per_candidate))
+	winner = candidates[winner_index]
+	print('Winner: ', winner)
+	print('--------------------------')
+
+
+
